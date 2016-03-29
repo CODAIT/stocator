600e2c656e56ea83edffcb88aa3df46dbf59d4a8
Daniel Jalova <djalova@us.ibm.com>
2016-03-29 10:53:35 -0700

    Split >5GB file uploads (WIP)

--- src/main/java/com/ibm/stocator/fs/swift/SwiftAPIClient.java
+++ src/main/java/com/ibm/stocator/fs/swift/SwiftAPIClient.java
@@ -118,21 +118,10 @@ public class SwiftAPIClient implements IStoreClient {
    */
   private boolean fModeAutomaticDelete;
 
-  /*
-   * Contains map of object names that were written by Spark.
-   * Used in container listing
-   */
   private Map<String, Boolean> cachedSparkOriginated;
 
-  /*
-   * Contains map of objects that were created by successfull Spark jobs.
-   * Used in container listing
-   */
   private Map<String, Boolean> cachedSparkJobsStatus;
 
-  /*
-   * Page size for container listing
-   */
   private final int pageListSize = 100;
 
   /**
@@ -275,7 +264,7 @@ public class SwiftAPIClient implements IStoreClient {
           isDirectory = true;
         }
       }
-      LOG.trace("Got object. isDirectory: {}  lastModified: {}", isDirectory, lastModified);
+      LOG.debug("Got object. isDirectory: {}  lastModified: {}", isDirectory, lastModified);
       return new FileStatus(contentLength, isDirectory, 1, blockSize,
               getLastModified(lastModified), path);
     }
@@ -291,7 +280,7 @@ public class SwiftAPIClient implements IStoreClient {
   }
 
   /**
-   * Transforms last modified time stamp from String to the long format
+   * Transform last modified time stamp to long format
    *
    * @param strTime time in string format as returned from Swift
    * @return time in long format
@@ -393,16 +382,16 @@ public class SwiftAPIClient implements IStoreClient {
             continue;
           } else {
             // if we here - data created by spark and job completed successfully
-            // however there be might parts of failed tasks that were not aborted
+            // however there might parts of failed tasks that were not aborted
             // we need to make sure there are no failed attempts
             if (nameWithoutTaskID(tmp.getName())
                 .equals(nameWithoutTaskID(previousElement.getName()))) {
               // found failed that was not aborted.
-              LOG.trace("Collisiion found between {} and {}", previousElement.getName(),
+              LOG.debug("Collisiion found between {} and {}", previousElement.getName(),
                   tmp.getName());
               setCorrectSize(tmp, cObj);
               if (previousElement.getContentLength() < tmp.getContentLength()) {
-                LOG.trace("New canditate is {}. Removed {}", tmp.getName(),
+                LOG.debug("New canditate is {}. Removed {}", tmp.getName(),
                     previousElement.getName());
                 previousElement = tmp.getAsObject();
               }
@@ -554,7 +543,7 @@ public class SwiftAPIClient implements IStoreClient {
   /**
    * Checks if container/object contains
    * container/object/_SUCCESS
-   * If so, this object was created by successful Hadoop job
+   * If so, this object belongs to successful Hadoop job
    *
    * @param objectName
    * @return boolean if job is successful
@@ -606,11 +595,11 @@ public class SwiftAPIClient implements IStoreClient {
 
   /**
    * Accepts any object name.
-   * If object name is of the form
-   * a/b/c/m.data/part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c
+   * If object name of the form
+   * a/b/c/gil.data/part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c
    *    .csv-attempt_20160317132a_wrong_0000_m_000000_1
-   * Then a/b/c/m.data/part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c.csv is returned.
-   * Perform test that attempt_20160317132a_wrong_0000_m_000000_1 is valid
+   * Then a/b/c/gil.data/part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c.csv is returned.
+   * Code testing that attempt_20160317132a_wrong_0000_m_000000_1 is valid
    * task id identifier
    *
    * @param objectName
--- src/main/java/com/ibm/stocator/fs/swift/SwiftOutputStream.java
+++ src/main/java/com/ibm/stocator/fs/swift/SwiftOutputStream.java
@@ -21,6 +21,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,6 +56,9 @@ public class SwiftOutputStream extends OutputStream {
    * HTTP connection object
    */
   private HttpURLConnection mHttpCon;
+  private int totalBytesWritten = 0;
+  private static final int MAX_PARTITION_SIZE = 512 * 1024;
+  private int partitionCount = 0;
 
   /**
    * Default constructor
@@ -59,6 +66,7 @@ public class SwiftOutputStream extends OutputStream {
    * @param httpCon URL connection
    * @throws IOException if failed to connect
    */
+
   public SwiftOutputStream(HttpURLConnection httpCon) throws IOException {
     try {
       httpCon.setDoInput(true);
@@ -78,29 +86,40 @@ public class SwiftOutputStream extends OutputStream {
   @Override
   public void write(int b) throws IOException {
     mOutputStream.write(b);
+    LOG.info("write1 called");
   }
 
   @Override
   public void write(byte[] b, int off, int len) throws IOException {
+    if (totalBytesWritten + len >= MAX_PARTITION_SIZE) {
+      splitFileUpload();
+      totalBytesWritten = 0;
+    }
     mOutputStream.write(b, off, len);
+    totalBytesWritten += len;
+    //LOG.info("{} bytes written", totalBytesWritten);
   }
 
   @Override
   public void write(byte[] b) throws IOException {
     mOutputStream.write(b);
+    LOG.info("writes2 called");
   }
 
   @Override
   public void close() throws IOException {
     mOutputStream.close();
+    LOG.info("{} bytes written", totalBytesWritten);
     InputStream is = null;
     try {
       // Status 400 and up should be read from error stream
       // Expecting here 201 Create or 202 Accepted
       if (mHttpCon.getResponseCode() >= 400) {
         is = mHttpCon.getErrorStream();
+        LOG.info("Error code: {}", mHttpCon.getResponseCode());
       } else {
         is = mHttpCon.getInputStream();
+        LOG.info("No error");
       }
       is.close();
     } catch (Exception e) {
@@ -117,4 +136,53 @@ public class SwiftOutputStream extends OutputStream {
   public void flush() throws IOException {
     mOutputStream.flush();
   }
+
+  private void splitFileUpload() throws IOException {
+    mOutputStream.close();
+    URL oldURL = mHttpCon.getURL();
+    String objName = oldURL.getPath() + Integer.toString(++partitionCount);
+    URL newURL = new URL(oldURL.getProtocol() + "://" + oldURL.getAuthority() + objName);
+
+    LOG.info("New URL path: {} {} {}", newURL.getProtocol(),
+            newURL.getAuthority(), objName);
+
+    try {
+      mHttpCon.disconnect();
+      HttpURLConnection newConn = (HttpURLConnection) newURL.openConnection();
+
+      newConn.setDoInput(true);
+      newConn.setRequestMethod("PUT");
+
+      newConn.setReadTimeout(READ_TIMEOUT);
+      newConn.setChunkedStreamingMode(STREAMING_CHUNK);
+
+      Set<Map.Entry<String, List<String>>> properties = mHttpCon.getRequestProperties().entrySet();
+      for (Map.Entry<String, List<String>> property : properties) {
+        for (String value : property.getValue()) {
+          LOG.info("Key: {} Value: {}", property.getKey(), value);
+          if (!(property.getKey().contains("POST") || property.getKey().contains("PUT"))) {
+            newConn.setRequestProperty(property.getKey(), value);
+            LOG.info("Setting requests key: {} value: {}", property.getKey(), value);
+          }
+        }
+      }
+      newConn.setDoOutput(true);
+
+      LOG.info("Settings configured");
+
+      Set<Map.Entry<String, List<String>>> debugProps = newConn.getRequestProperties().entrySet();
+      for (Map.Entry<String, List<String>> property : debugProps) {
+        for (String value : property.getValue()) {
+          LOG.info("Key: {} set to value: {}", property.getKey(), value);
+        }
+      }
+
+      mOutputStream = newConn.getOutputStream();
+      mHttpCon = newConn;
+    } catch (IOException e) {
+      LOG.error(e.getMessage());
+      throw e;
+    }
+  }
+
 }
