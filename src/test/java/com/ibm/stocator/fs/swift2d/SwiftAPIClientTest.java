a755bfab2d143cd37fc91288d3c4a8e7be731ec6
Jason Lee <cjlee@us.ibm.com>
2016-05-06 15:16:19 -0700

    added several test cases for SwiftAPIClient

--- src/test/java/com/ibm/stocator/fs/swift2d/SwiftAPIClientTest.java
+++ src/test/java/com/ibm/stocator/fs/swift2d/SwiftAPIClientTest.java
@@ -17,21 +17,49 @@
 
 package com.ibm.stocator.fs.swift2d;
 
+import org.javaswift.joss.model.StoredObject;
+import java.util.HashMap;
+import org.javaswift.joss.client.mock.ContainerMock;
+import org.javaswift.joss.client.mock.StoredObjectMock;
+import org.javaswift.joss.client.mock.AccountMock;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.reflect.Whitebox;
-
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileStatus;
 import com.ibm.stocator.fs.swift.SwiftAPIClient;
 
+import static com.ibm.stocator.fs.common.Constants.HADOOP_SUCCESS;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(StoredObject.class)
 public class SwiftAPIClientTest {
 
   private SwiftAPIClient mSwiftAPIClient;
+  private StoredObjectMock mStoredObject;
+  private AccountMock mAccount;
+  private ContainerMock mContainer;
+  private HashMap mHashMap;
+  private String mContainerName;
 
   @Before
   public final void before() {
     mSwiftAPIClient = PowerMockito.mock(SwiftAPIClient.class);
+    mAccount = new AccountMock();
+    mContainerName = "aa-bb-cc";
+    mContainer = (ContainerMock)new ContainerMock(mAccount, mContainerName).create();
+    mHashMap = PowerMockito.spy(new HashMap<String, Boolean>());
+
+    Whitebox.setInternalState(mSwiftAPIClient, "cachedSparkJobsStatus", mHashMap);
+    Whitebox.setInternalState(mSwiftAPIClient, "cachedSparkOriginated", mHashMap);
+    Whitebox.setInternalState(mSwiftAPIClient, "container", mContainerName);
+    Whitebox.setInternalState(mSwiftAPIClient, "mAccount", mAccount);
   }
 
   @Test
@@ -40,23 +68,27 @@ public class SwiftAPIClientTest {
 
     String input = objectUnified;
     String result = Whitebox.invokeMethod(mSwiftAPIClient, "extractUnifiedObjectName", input);
-    Assert.assertEquals(input, result);
+    Assert.assertEquals("extractUnifiedObjectName() shows incorrect name",
+            input, result);
 
     input = objectUnified + "/_SUCCESS";
     result = Whitebox.invokeMethod(mSwiftAPIClient, "extractUnifiedObjectName", input);
-    Assert.assertEquals(objectUnified, result);
+    Assert.assertEquals("extractUnifiedObjectName() shows incorrect name with _SUCCESS",
+            objectUnified, result);
 
     input = objectUnified + "/"
         + "part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c.csv-"
         + "attempt_201603171328_0000_m_000000_1";
     result = Whitebox.invokeMethod(mSwiftAPIClient, "extractUnifiedObjectName", input);
-    Assert.assertEquals(objectUnified, result);
+    Assert.assertEquals("extractUnifiedObjectName() shows incorrect name with attempt",
+            objectUnified, result);
 
     input = "a/b/c/gil.data/"
         + "part-r-00000-48ae3461-203f-4dd3-b141-a45426e2d26c.csv-"
         + "attempt_20160317132a_wrong_0000_m_000000_1";
     result = Whitebox.invokeMethod(mSwiftAPIClient, "extractUnifiedObjectName", input);
-    Assert.assertEquals(input, result);
+    Assert.assertEquals("extractUnifiedObjectName() shows incorrect name with wrong taskAttemptID",
+            input, result);
   }
 
   @Test
@@ -68,11 +100,137 @@ public class SwiftAPIClientTest {
     input = objectName
             + "-attempt_201603171328_0000_m_000000_1";
     String result = Whitebox.invokeMethod(mSwiftAPIClient, "nameWithoutTaskID", input);
-    Assert.assertEquals(objectName, result);
+    Assert.assertEquals("nameWithoutTaskID() shows incorrect name",
+            objectName, result);
 
     input = objectName
             + "attempt_20160317132a_wrong_0000_m_000000_1";
     result = Whitebox.invokeMethod(mSwiftAPIClient, "nameWithoutTaskID", input);
-    Assert.assertEquals(input, result);
+    Assert.assertEquals("nameWithoutTaskID() shows incorrect name with wrong taskAttemptID",
+            input, result);
+  }
+
+  @Test
+  public void getMergedPathTest() throws Exception {
+    String hostName = "swift2d://aa-bb-cc.lvm/";
+    String hostName2 = "swift2d://aa-bb-cc-dd.lvm/";
+    String pathName = "data7-1-23-a.txt";
+    String objectName = "data7-1-23-a.txt/part-00002-attempt_201612062056_0000_m_000002_2";
+
+    //test when the objectName passed in is the same as the pathName
+    String result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(hostName + pathName), pathName);
+    Assert.assertEquals("getMergedPath() shows incorrect merged name "
+            + "when the object name is the same as the path name",
+            hostName + pathName, result);
+
+    //test when the objectName starts with the pathName
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(hostName + pathName), objectName);
+    Assert.assertEquals("getMergedPath() shows incorrect merged name "
+            + "when the object name starts with the path name",
+            hostName + objectName, result);
+
+    //test when the objectName is not the same, nor does start with, the pathName
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(hostName + pathName), "ABC");
+    Assert.assertEquals("getMergedPath() shows incorrect merged name "
+            + "when the object name is not the same, nor does start with, the path name",
+            hostName + pathName, result);
+
+    //test when the hostName is not part of the Path
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(pathName), objectName);
+    Assert.assertEquals("getMergedPath() shows incorrect merged name "
+            + "when the host name is not part of the Path",
+            hostName + objectName, result);
+
+    //test when a different hostName is part of the Path
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(hostName2 + pathName), objectName);
+    Assert.assertEquals("getMergedPath() shows incorrect merged name "
+            + "when a different host name is part of the Path",
+            hostName + objectName, result);
+  }
+
+  @Test
+  public void getLastModifiedTest() throws Exception {
+    String stringTime = "Fri, 06 May 2016 03:44:47 GMT";
+    long longTime = 1462506287000L;
+
+    long result = Whitebox.invokeMethod(mSwiftAPIClient, "getLastModified", stringTime);
+    Assert.assertEquals("getLastModified() shows incorrect time",
+            longTime, result);
+  }
+
+  @Test
+  public void isJobSuccessfulTest() throws Exception {
+    String objectName = "data7-1-23-a.txt/part-00002-attempt_201612062056_0000_m_000002_2";
+
+    //test to see if _SUCCESS object does not exist
+    mStoredObject = new StoredObjectMock(mContainer, objectName + "/" + HADOOP_SUCCESS);
+    Assert.assertEquals("isJobSuccessful() shows _SUCCESS object already exists",
+            false, mStoredObject.exists());
+
+    //test to see if _SUCCESS object exists
+    mStoredObject.uploadObject(new byte[]{});
+    Assert.assertEquals("isJobSuccessful() shows _SUCCESS object does not exist",
+            true, mStoredObject.exists());
+
+    //test when _HADOOP_SUCCESS exists
+    boolean result = Whitebox.invokeMethod(mSwiftAPIClient, "isJobSuccessful", objectName);
+    Assert.assertEquals("isJobSuccessful() failed even when HADOOP_SUCCESS exists",
+            true, result);
+
+    //test to see if job status is cached properly
+    HashMap jobStatus = Whitebox.getInternalState(mSwiftAPIClient, "cachedSparkJobsStatus");
+    Assert.assertEquals("isJobSuccessful() shows job status is not cached correctly",
+            true, jobStatus.containsKey(objectName));
+
+    //test to see if it obtains status from the cache
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "isJobSuccessful", objectName);
+    Assert.assertEquals("isJobSuccessful() shows job status is not obtained from cache",
+            true, result);
+
+    //test when the container name is part of the object name
+    result = Whitebox.invokeMethod(mSwiftAPIClient, "isJobSuccessful",
+            mContainerName + "/" + objectName);
+    Assert.assertEquals("isJobSuccessful() failed when the container name"
+            + "is part of the object name",
+            true, result);
+  }
+
+  @Test
+  public void setCorrectSizeTest() throws Exception {
+    String objectName = "data7-1-23-a.txt/part-00002-attempt_201612062056_0000_m_000002_2";
+    mStoredObject = new StoredObjectMock(mContainer, objectName);
+    mStoredObject.uploadObject(new byte[]{1,2,3});
+
+    //test to see if correct size is returned
+    Whitebox.invokeMethod(mSwiftAPIClient, "setCorrectSize",
+            mStoredObject, mContainer);
+    Assert.assertEquals("setCorrectSize() shows incorrect size returned",
+            3, mStoredObject.getContentLength());
+  }
+
+  @Test
+  public void getFileStatusTest() throws Exception {
+    String objectName = "data7-1-23-a.txt/part-00002-attempt_201612062056_0000_m_000002_2";
+    String hostName = "swift2d://aa-bb-cc.lvm/";
+    String pathName = "data7-1-23-a.txt";
+    mStoredObject = new StoredObjectMock(mContainer, objectName);
+    mStoredObject.uploadObject(new byte[]{1, 2, 3});
+
+    //test to see if correct length is returned
+    FileStatus fs = Whitebox.invokeMethod(mSwiftAPIClient, "getFileStatus",
+            mStoredObject, mContainer, hostName, new Path(pathName));
+    Assert.assertEquals("getFileStatus() shows incorrect length",
+            3, fs.getLen());
+
+    //test to see if correct path is returned
+    String result = Whitebox.invokeMethod(mSwiftAPIClient, "getMergedPath",
+            hostName, new Path(pathName), objectName);
+    Assert.assertEquals("getFileStatus() shows incorrect path",
+            new Path(result), fs.getPath());
   }
 }
