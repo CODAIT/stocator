fd8937e00cdc04c8bbedda873e626517ad588984
Paul Carron <paul.carron@anaeko.com>
2017-11-27 21:26:16 +0000

    git issue 154 dots in bucket name

--- src/main/java/com/ibm/stocator/fs/common/Utils.java
+++ src/main/java/com/ibm/stocator/fs/common/Utils.java
@@ -82,7 +82,7 @@ public class Utils {
    */
   public static String getContainerName(String hostname,
       boolean serviceRequired) throws IOException {
-    int i = hostname.indexOf(".");
+    int i = hostname.lastIndexOf(".");
     if (i <= 0) {
       if (serviceRequired) {
         throw badHostName(hostname);
@@ -100,7 +100,7 @@ public class Utils {
    * @throws IOException if the hostname was invalid
    */
   public static String getServiceName(String hostname) throws IOException {
-    int i = hostname.indexOf(".");
+    int i = hostname.lastIndexOf(".");
     if (i <= 0) {
       throw badHostName(hostname);
     }
@@ -120,7 +120,7 @@ public class Utils {
    * @throws IOException if the hostname was invalid
    */
   public static String getServiceName(String hostname, String defaultService) throws IOException {
-    int i = hostname.indexOf(".");
+    int i = hostname.lastIndexOf(".");
     if (i <= 0) {
       throw badHostName(hostname);
     }
@@ -141,7 +141,7 @@ public class Utils {
     LOG.trace("Checking schema {}", uri.toString());
     String hostName = Utils.getHost(uri);
     LOG.trace("Got hostname as {}", hostName);
-    int i = hostName.indexOf(".");
+    int i = hostName.lastIndexOf(".");
     if (i < 0) {
       return false;
     }
