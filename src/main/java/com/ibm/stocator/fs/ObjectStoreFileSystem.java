cb12abe5caa7255000873778ed53609213bc0451
Gil Vernik <gilv@il.ibm.com>
2018-08-24 06:55:54 +0300

    [cos] enable rename for objects

--- src/main/java/com/ibm/stocator/fs/ObjectStoreFileSystem.java
+++ src/main/java/com/ibm/stocator/fs/ObjectStoreFileSystem.java
@@ -474,7 +474,7 @@ public class ObjectStoreFileSystem extends ExtendedFileSystem {
       FSDataOutputStream outStream = storageClient.createObject(plainObjName,
           Constants.APPLICATION_DIRECTORY, metadata, statistics);
       outStream.close();
-    } else if (f.getName().equals("_spark_metadata")) {
+    } else {
       LOG.debug("mkdirs on non temp object. Create {}", f.toString());
       String objName = stocatorPath.getObjectNameRoot(f, false, storageClient.getDataRoot(),
           true);
